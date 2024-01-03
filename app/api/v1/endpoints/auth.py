24ab3b8f69cd6e19c7506314d4b578ea9d9b08c0
Naman Bhatia <bhatia.naman@hotmail.com>
2024-01-04 01:00:00 +0530

    chore: restructured and removed redundant code

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -1,111 +1,26 @@
 from typing import Annotated
 
-from fastapi import Depends, APIRouter, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import JWTError, jwt
-from passlib.context import CryptContext
-from datetime import datetime, timedelta
+from fastapi import APIRouter
 from app.core.database import get_db
-from app.models.user import User, UserInDB, UserCreate
+from app.models.user import User, UserCreate
 from app.core.config import settings
 from app.schemas.token import Token, TokenData
 
-router = APIRouter()
-#
-# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-#
-# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-#
-# async def get_current_user(token: str = Depends(oauth2_scheme), db = Depends(get_db)):
-#     credentials_exception = HTTPException(
-#         status_code=status.HTTP_401_UNAUTHORIZED,
-#         detail="Could not validate credentials",
-#         headers={"WWW-Authenticate": "Bearer"},
-#     )
-#     try:
-#         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
-#         email: str = payload.get("sub")
-#         if email is None:
-#             raise credentials_exception
-#         token_data = TokenData(email=email)
-#     except JWTError:
-#         raise credentials_exception
-#     user = await db["users"].find_one({"email": token_data.email})
-#     if user is None:
-#         raise credentials_exception
-#     return user
-#
-#
-# async def get_current_active_user(
-#     current_user: Annotated[User, Depends(get_current_user)]
-# ):
-#     if current_user.disabled:
-#         raise HTTPException(status_code=400, detail="Inactive user")
-#     return current_user
-#
-# def verify_password(plain_password, hashed_password):
-#     return pwd_context.verify(plain_password, hashed_password)
-#
-# def get_password_hash(password):
-#     return pwd_context.hash(password)
-#
-# async def authenticate_user(db, email: str, password: str):
-#     user = await db["users"].find_one({"email": email})
-#     if not user:
-#         return False
-#     if not verify_password(password, user["hashed_password"]):
-#         return False
-#     return user
-#
-# def create_access_token(data: dict, expires_delta: timedelta = None):
-#     to_encode = data.copy()
-#     if expires_delta:
-#         expire = datetime.utcnow() + expires_delta
-#     else:
-#         expire = datetime.utcnow() + timedelta(minutes=15)
-#     to_encode.update({"exp": expire})
-#     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
-#     return encoded_jwt
-#
-# @router.post("/auth/signup", response_model=UserDetails)
-# async def create_user(user: User, db = Depends(get_db)):
-#     db_user = await db["users"].find_one({"email": user.email})
-#     if db_user:
-#         raise HTTPException(status_code=400, detail="Email already registered")
-#     hashed_password = get_password_hash(user.password)
-#     db_user = await db["users"].insert_one({"email": user.email, "hashed_password": hashed_password})
-#     return User(email=user.email, id=str(db_user.inserted_id))
-#
-# @router.post("/auth/login", response_model=Token)
-# async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db = Depends(get_db)):
-#     user = await authenticate_user(db, form_data.username, form_data.password)
-#     if not user:
-#         raise HTTPException(
-#             status_code=status.HTTP_401_UNAUTHORIZED,
-#             detail="Incorrect email or password",
-#             headers={"WWW-Authenticate": "Bearer"},
-#         )
-#     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-#     access_token = create_access_token(
-#         data={"sub": user["email"]}, expires_delta=access_token_expires
-#     )
-#     return {"access_token": access_token, "token_type": "bearer"}
-
-
 from datetime import datetime, timedelta
 
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 
 from app.models.user import UserInDB
 
+router = APIRouter()
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/token")
 
-app = FastAPI()
+
 
 
 def verify_password(plain_password, hashed_password):
